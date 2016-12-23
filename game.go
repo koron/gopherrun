6151656d8a9eb2a30d944a8ae9f8f3eb8bb7b29c
MURAOKA Taro <koron.kaoriya@gmail.com>
2016-12-24 01:57:07 +0900

    implement more

--- game.go
+++ game.go
@@ -8,10 +8,32 @@ import (
 var (
 	// scw is screen character width
 	scw = (screenWidth+cellWidth-1)/cellWidth + 1
+
 	// sch is screen character height
-	sch    = (screenHeight+cellHeight-1)/cellHeight + 1
+	sch = (screenHeight+cellHeight-1)/cellHeight + 1
+
 	// maxBgOffx is max value for bgOffX
 	maxBgOffx = fixed.I(16)
+
+	// gravityPower
+	gravityPower = fixed.I(3) / 2
+
+	maxSpeedY = fixed.I(16) / 2
+
+	// risingPower
+	risingPower = fixed.I(11) / 2
+
+	risingInitN = 10
+
+	gopherX = fixed.I(320) / 5
+
+	initSpeedX = fixed.I(1) / 3
+
+	maxSpeedX = fixed.I(6) / 2
+
+	accelX = fixed.I(1) / 40
+
+	gopherInitY = fixed.I(8 * 16)
 )
 
 type Game struct {
@@ -20,29 +42,78 @@ type Game struct {
 	ch1 *sdl.Texture
 	ch2 *sdl.Texture
 
-	running  bool
-	frameNum uint64
-	pressedA bool
-	pressedB bool
+	running   bool
+	frameNum  uint64
+	pressedA  bool
+	releasedA bool
+	pressedB  bool
 
 	bgMap  []uint8
 	bgOffX fixed.Int26_6
 	bgOffY fixed.Int26_6
 
-	speedX fixed.Int26_6
+	spPatterns []SpritePattern
+	sprites    []Sprite
+
+	mode     Mode
+	gopherY  fixed.Int26_6
+	speedX   fixed.Int26_6
+	speedY   fixed.Int26_6
+	floating bool
+	risingN  int
 }
+
+type Sprite struct {
+	id int
+	x  int32
+	y  int32
+}
+
+type SpritePattern struct {
+	x int32
+	y int32
+	w int32
+	h int32
+}
+
+type Mode int
+
+const (
+	title Mode = iota
+	playing
+	gameover
+)
 
 // Init initialize all game status.
 func (g *Game) Init() error {
 	g.bgMap = make([]uint8, scw*sch)
+	g.spPatterns = []SpritePattern{
+		SpritePattern{x: 0, y: 0, w: 16, h: 32},
+	}
+	g.sprites = []Sprite{
+		Sprite{id: 0, x: int32(gopherX.Floor()), y: 0},
+	}
+	g.gotoTitle()
+	return nil
+}
+
+func (g *Game) gotoTitle() {
 	for x := 0; x < scw; x++ {
+		for y := 0; y < 10; y++ {
+			g.bgMap[x*sch+y] = 0x00
+		}
 		for y := 10; y < sch; y++ {
-			g.bgMap[x*sch+y] = uint8(x%16 + 16)
+			g.bgMap[x*sch+y] = 0x10
 		}
 	}
+	g.bgOffX = 0
+	g.mode = title
+	g.gopherY = gopherInitY
 	g.running = true
-	g.speedX = fixed.I(1) / 3
-	return nil
+	g.speedX = initSpeedX
+	g.speedY = 0
+	g.floating = false
+	g.risingN = 0
 }
 
 func (g *Game) Run() error {
@@ -84,6 +155,7 @@ func (g *Game) drawBG() error {
 func (g *Game) initNewFrame() {
 	g.frameNum++
 	g.pressedA = false
+	g.releasedA = false
 	g.pressedB = false
 }
 
@@ -101,14 +173,11 @@ func (g *Game) procEvent(raw sdl.Event) {
 		case sdl.K_ESCAPE:
 			g.running = false
 		}
-	}
-}
-
-func (g *Game) update() {
-	g.bgOffX += g.speedX
-	for g.bgOffX >= maxBgOffx {
-		g.bgOffX -= maxBgOffx
-		g.shiftBG()
+	case *sdl.KeyUpEvent:
+		switch ev.Keysym.Sym {
+		case sdl.K_SPACE, sdl.K_RETURN:
+			g.releasedA = true
+		}
 	}
 }
 
@@ -117,9 +186,130 @@ func (g *Game) shiftBG() {
 	copy(g.bgMap[0:l-sch], g.bgMap[sch:])
 }
 
+func (g *Game) drawSprites() error {
+	for i := len(g.sprites) - 1; i >= 0; i-- {
+		s := g.sprites[i]
+		p := g.spPatterns[s.id]
+		src := sdl.Rect{p.x, p.y, p.w, p.h}
+		dst := sdl.Rect{s.x, s.y, p.w, p.h}
+		if err := g.ren.Copy(g.ch2, &src, &dst); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
 func (g *Game) render() error {
 	if err := g.drawBG(); err != nil {
 		return err
 	}
+	if err := g.drawSprites(); err != nil {
+		return err
+	}
 	return nil
+}
+
+func (g *Game) update() {
+	if g.floating {
+		if g.risingN > 0 {
+			if g.releasedA {
+				g.risingN = 0
+			} else {
+				g.risingN--
+			}
+			g.speedY = -risingPower
+		} else {
+			g.speedY += gravityPower
+			if g.speedY > maxSpeedY {
+				g.speedY = maxSpeedY
+			}
+		}
+	} else {
+		if g.pressedA {
+			g.floating = true
+			g.risingN = risingInitN
+			g.speedY = -risingPower
+			g.mode = playing
+		}
+	}
+
+	switch g.mode {
+	case title:
+		g.speedX = initSpeedX
+	case playing:
+		g.speedX += accelX
+		if g.speedX > maxSpeedX {
+			g.speedX = maxSpeedX
+		}
+	case gameover:
+		g.speedX = 0
+	}
+	// TODO: check to hit wall
+	g.bgOffX += g.speedX
+	for g.bgOffX >= maxBgOffx {
+		g.bgOffX -= maxBgOffx
+		g.shiftBG()
+		// insert new bgMap at right
+		switch g.mode {
+		case title:
+			n := (scw - 1) * sch
+			for y := 0; y < sch; y++ {
+				if y < 10 {
+					g.bgMap[n+y] = 0x00
+				} else {
+					g.bgMap[n+y] = 0x11
+				}
+			}
+		case playing:
+			// TODO: generate stage data
+			n := (scw - 1) * sch
+			for y := 0; y < sch; y++ {
+				if y < 10 {
+					g.bgMap[n+y] = 0x00
+				} else {
+					g.bgMap[n+y] = 0x00
+				}
+			}
+		}
+	}
+
+	g.gopherY += g.speedY
+	// check to touch grand
+	if g.speedY >= 0 {
+		x := (gopherX + g.bgOffX).Floor()
+		cy := (g.gopherY.Floor() + cellHeight*2) / cellHeight
+		cx := x / cellWidth
+		cw := 2
+		if x%cellWidth == 0 {
+			cw = 1
+		}
+		if cy < sch {
+			touch := false
+			for i := 0; i < cw; i++ {
+				if g.bgMap[(cx+i)*sch+cy] >= 0x10 {
+					touch = true
+					break
+				}
+			}
+			if touch {
+				g.gopherY = fixed.I((cy - 2) * cellHeight)
+				g.speedY = 0
+				g.floating = false
+				g.risingN = 0
+			} else {
+				g.floating = true
+			}
+		}
+	}
+	if g.gopherY.Floor() > screenHeight {
+		// game over
+		g.mode = gameover
+	}
+
+	if g.mode == gameover && g.pressedA {
+		// back to title
+		g.gotoTitle()
+	}
+
+	g.sprites[0].y = int32(g.gopherY.Floor())
 }
